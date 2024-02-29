3d169b9e8b0941ad917b50e7ba5b23e46e03ae2d
zain-tx <zain@techcreatix.com>
2024-03-01 03:49:44 +0500

    fix ip

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,18 @@
 class HomeController < ApplicationController
      def index
-          if current_user.admin?
-               @session = User.where(role: "user").order(created_at: :desc)
+          allowed_ip = "::1"
+          client_ip = request.remote_ip
+          if client_ip != allowed_ip
+               file_path = Rails.root.join('app', 'views', 'devise', 'invalid_ip.html.erb')
+               html_content = File.read(file_path)
+               render html: html_content.html_safe
           else
-               @session = current_user.attendances.order(created_at: :desc)
-               @user = User.find(current_user.id)
+               if current_user.admin?
+                    @session = User.where(role: "user").order(created_at: :desc)
+               else
+                    @session = current_user.attendances.order(created_at: :desc)
+                    @user = User.find(current_user.id)
+               end
           end
      end
 end
