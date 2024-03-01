06bd5a4ab06075cd9470cf636f54ccfe0b21f0df
zain-tx <zain@techcreatix.com>
2024-03-01 14:53:43 +0500

    fix address

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
      def index
           allowed_ip = ENV['IP_ADDRESS']
           client_ip = request.remote_ip
-          unless allowed_ip.include?(client_ip)
+          unless allowed_ip.split(',').include?(client_ip)
                file_path = Rails.root.join('app', 'views', 'devise', 'invalid_ip.html.erb')
                html_content = File.read(file_path)
                render html: html_content.html_safe
