846f15c6b2753b05a1f1f44d0d3af2578f263bc4
zain-tx <zain@techcreatix.com>
2024-03-04 22:49:12 +0500

    slack service on

--- app/controllers/attendance_controller.rb
+++ app/controllers/attendance_controller.rb
@@ -13,7 +13,7 @@ class AttendanceController < ApplicationController
             @session.check_in_time = Time.now.utc
             @session.save!
             flash[:success] = "Checked IN successfully"
-          #   SlackService.new(current_user, "Checked In", @session.check_in_time).send_message
+            SlackService.new(current_user, "Checked In", @session.check_in_time).send_message
             redirect_to root_path
           else
                flash[:alert] = "Already Checked In"
@@ -29,7 +29,7 @@ class AttendanceController < ApplicationController
                session.update!(total_hours: duration_seconds)
                end
             flash[:success] = "Checked OUT successfully"
-               # SlackService.new(current_user, "Checked Out", @session.last.check_out_time).send_message
+               SlackService.new(current_user, "Checked Out", @session.last.check_out_time).send_message
                redirect_to root_path
      end
 
