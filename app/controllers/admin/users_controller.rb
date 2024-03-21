9ff81b5939ab97ea2cef5b5b970350a704bb48f5
zain-tx <zain@techcreatix.com>
2024-03-21 16:16:40 +0500

    fix side bar

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -67,7 +67,7 @@ class Admin::UsersController < ApplicationController
       date_range = (start_date.to_date..end_date.to_date).to_a
       date_range.reject! { |date| date.saturday? || date.sunday? }
       present_dates = @user_sessions.pluck(:check_in_time).map(&:to_date)
-      @leaves = date_range.count { |date| !present_dates.include?(date) }
+      @leaves = date_range.count { |date| !present_dates.include?(date) && date < Date.today }      
       if @user_sessions.present?
         total_hrs = 0
         @user_sessions.each do |attendance|
