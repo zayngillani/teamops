be1232327aa6fdcbf5969a48561fd88b858054b0
zain-tx <zain@techcreatix.com>
2024-05-03 20:01:17 +0500

    clear seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-user= User.where(role: "user").pluck(:supervisor)
-user.update(supervisor: "Fahad")
