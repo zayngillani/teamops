fdd436175d3564f650d2fcbfb9765390bc9f0748
zain-tx <zain@techcreatix.com>
2024-05-18 18:35:44 +0500

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: "admin@techcreatix.com", name: "Techcreatix", password: "Techcre@tix@786", password_confirmation: "Techcre@tix@786", role: "admin")
