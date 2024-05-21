7dfc2624c61ffb802f571ffaac52afde38c34558
zain-tx <zain@techcreatix.com>
2024-05-21 21:33:03 +0500

    leave slack alerts

--- app/service/slack_service.rb
+++ app/service/slack_service.rb
@@ -3,7 +3,7 @@ class SlackService
           @user = user
           @time = time
           @message = message
-          if @user.email.ends_with?("@gmail.com")
+          if @user.email.ends_with?("@gmail.com") || !@time.is_a?(Time)
                @channel = ENV["TEST_CHANNEL"]
           else
                @channel = ENV["SLACK_CHANNEL"]
@@ -12,6 +12,11 @@ class SlackService
      end
 
      def send_message
-       @client.chat_postMessage(channel: @channel,text: " <@#{@user.slack_member_id}> #{@message} at #{@time.in_time_zone('Asia/Karachi').strftime("%b %d, %I:%M%p %Z")}")
+          @client.chat_postMessage(channel: @channel,text: " <@#{@user.slack_member_id}> #{@message} at #{@time.in_time_zone('Asia/Karachi').strftime("%b %d, %I:%M%p %Z")}")
+     end
+     
+     def send_leave
+       @request_user = User.find_by(id: @time.user_id)
+       @client.chat_postMessage(channel: @channel,text: " <@#{@request_user.slack_member_id}> #{@message} #{@time.status == "pending" ? @time.start_date.strftime("%d/%B")..@time.end_date.strftime("%d/%B") : "<@#{@user.name}>"}")
      end
 end
