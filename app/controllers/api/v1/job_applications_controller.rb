f793dba4f384218c987849120b387589d1bdcb30
jahanzaib-tx <jahanzaib.javed@techcreatix.com>
2024-07-08 18:07:46 +0500

    updated changes in after integartion and testin with front end

--- app/controllers/api/v1/job_applications_controller.rb
+++ app/controllers/api/v1/job_applications_controller.rb
@@ -8,18 +8,18 @@ module Api
       skip_before_action :authenticate_user!, only: [:create]
 
       def create
-        job_application = JobApplication.new(job_application_params)
+        @job_application = JobApplication.new(job_application_params)
 
-        if params[:job_application][:resume].present?
-          if job_application.save
+        if params[:resume].present?
+          if @job_application.save
             # Call the Slack notification service
-            JobApplicationSlackService.new(@job_application).notify_submission
+            JobApplicationSlackService.new(@job_application, params[:job_title], params[:resume]).notify_submission
             # Upload resume to FTP
-            upload_to_ftp(params[:job_application][:resume].tempfile)
+            upload_to_ftp(params[:resume])
 
-            render json: { status: 'SUCCESS', message: 'Job application submitted', data: job_application }, status: :ok
+            render json: { status: 'SUCCESS', message: 'Job application submitted', data: @job_application }, status: :ok
           else
-            render json: { status: 'ERROR', message: 'Job application not saved', data: job_application.errors }, status: :unprocessable_entity
+            render json: { status: 'ERROR', message: 'Job application not saved', data: @job_application.errors }, status: :unprocessable_entity
           end
         else
           render json: { status: 'ERROR', message: 'Resume file not provided' }, status: :unprocessable_entity
@@ -29,7 +29,7 @@ module Api
       private
 
       def job_application_params
-        params.require(:job_application).permit(:name, :email, :qualification, :cnic, :current_experience, :contact_number, :current_salary, :expected_salary, :resume)
+        params.require(:job_application).permit(:name, :email, :qualification, :cnic, :current_experience, :contact_number, :current_salary, :expected_salary)
       end
 
       def upload_to_ftp(file)
@@ -38,13 +38,16 @@ module Api
           ftp.connect(ENV['FTP_HOST'], ENV['FTP_PORT'].to_i)
           ftp.login(ENV['FTP_USERNAME'], ENV['FTP_PASSWORD'])
           ftp.passive = true
-
-          remote_filename = File.basename(file.path)
+        
+          # Extract the original filename from the file object
+          original_filename = file.original_filename
+          remote_filename = original_filename
+        
           ftp.putbinaryfile(file.path, remote_filename)
-
+        
           ftp.close
-          job_application.update(resume_link: remote_filename)
-
+          @job_application.update(resume_link: remote_filename)
+        
           Rails.logger.info "Uploaded #{remote_filename} to FTP server"
         rescue StandardError => e
           Rails.logger.error "FTP upload failed: #{e.message}"
--- app/service/job_application_slack_service.rb
+++ app/service/job_application_slack_service.rb
@@ -1,27 +1,38 @@
 class JobApplicationSlackService
-  def initialize(job_application)
+  def initialize(job_application, job_title, resume_file)
+    @job_title = job_title
     @job_application = job_application
-    @slack_client = Slack::Web::Client.new
+    @resume_file = resume_file
+    @slack_client = Slack::Web::Client.new(token: ENV['SLACK_API_TOKEN'])
   end
 
   def notify_submission
-    message = build_message
-    @slack_client.chat_postMessage(channel: ENV['TEST_CHANNEL'], text: message)
+    file_url = "http/test/test-resume.pdf"
+    message = build_message(file_url)
+    post_message(message)
   end
 
   private
 
-  def build_message
+  def build_message(file_url)
     <<~MESSAGE
-      New job application submitted:
-      Name: #{@job_application.name}
-      Email: #{@job_application.email}
-      Qualification: #{@job_application.qualification}
-      Current Experience: #{@job_application.current_experience}
-      Contact Number: #{@job_application.contact_number}
-      Current Salary: #{@job_application.current_salary}
-      Expected Salary: #{@job_application.expected_salary}
-      Resume: #{@job_application.resume_link}
+      #{@job_application.name} applied for this job post #{@job_title}. Please find below resume of the candidate
+      - **Resume**: [View Resume](#{file_url})
     MESSAGE
   end
+
+  def post_message(message)
+    @slack_client.chat_postMessage(channel: ENV['TEST_CHANNEL'], text: message)
+  end
+
+  # def upload_resume_file
+  #   response = @slack_client.files_upload(
+  #     channels: ENV['TEST_CHANNEL'],
+  #     as_user: true,
+  #     file: Faraday::UploadIO.new(@resume_file.path, 'application/pdf'),
+  #     title: "#{@job_application.name}'s Resume",
+  #     filename: @resume_file.original_filename
+  #   )
+  #   response.file.permalink
+  # end
 end
