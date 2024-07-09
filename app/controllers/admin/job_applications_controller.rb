b477980ca66dc2ff28d1acacee0449c7facedc83
jahanzaib-tx <jahanzaib.javed@techcreatix.com>
2024-07-09 16:25:11 +0500

    updated access rights to the job applications page

--- app/controllers/admin/job_applications_controller.rb
+++ app/controllers/admin/job_applications_controller.rb
@@ -1,6 +1,12 @@
 class Admin::JobApplicationsController < ApplicationController
-  
+  before_action :authorize_admin!
+
   def index
     @job_applications = JobApplication.all
+  end
+
+  private
+  def authorize_admin!
+    redirect_to root_path, alert: 'Access Denied!' unless current_user.admin?
   end
 end
