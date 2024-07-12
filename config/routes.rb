5ee89119e9dbfb3cefbc492d15c3115b1525468c
jahanzaib-tx <jahanzaib.javed@techcreatix.com>
2024-07-12 16:01:32 +0500

    Feature-TTT-96-Added api for creating contact setails

--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :job_applications, only: [:create]
+      resources :contact_details, only: [:create]
       get '/job_post_list', :to => "job_applications#get_job_post_list", as: 'job_post_list'
     end
   end
