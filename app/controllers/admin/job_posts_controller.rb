02c142af7456308c001f870a21b00a8f32fe5852
jahanzaib-tx <jahanzaib.javed@techcreatix.com>
2024-07-18 18:58:58 +0500

    fixes in edit form and dropdown

--- app/controllers/admin/job_posts_controller.rb
+++ app/controllers/admin/job_posts_controller.rb
@@ -13,25 +13,28 @@ class Admin::JobPostsController < ApplicationController
 
   def create
     @job_post = JobPost.new(job_post_params)
-
+    # Custom handling of requirememnts and qualifications
     requirements_and_qualification = params.dig(:job_post, :requirements_and_qualification)&.split('\n')
     proper_requirements_and_qualification = requirements_and_qualification[0].split("\n")
-
     @job_post.requirements_and_qualification = proper_requirements_and_qualification
 
     if @job_post.save
       redirect_to admin_job_posts_path, notice: 'Job has been Published'
     else
-      flash.now[:error] = 'There was an error saving the job post'
-      render :new
+      redirect_to new_admin_job_post_path, error: 'There was an error saving the job post'
     end
   end
 
   def show
   end
 
+  def edit
+  end
+
+  def update
+  end
+
   def destroy
-    debugger
     @job_post.destroy
     redirect_to admin_job_posts_path, notice: 'Job has been deleted'
   end
@@ -43,7 +46,7 @@ class Admin::JobPostsController < ApplicationController
   end
 
   def job_post_params
-    params.require(:job_post).permit(:title, :details)
+    params.require(:job_post).permit(:title, :details, :requirements_and_qualification)
   end
   
 
