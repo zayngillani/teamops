b9861fed080dd83a5863993ab3cb4070cd095984
jahanzaib-tx <jahanzaib.javed@techcreatix.com>
2024-07-24 15:29:12 +0500

    scroll design fix and delete pop up design fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,26 @@
 module ApplicationHelper
 
      def flash_class(key)
-          case key
-          when 'success'
-          'alert-success'
-          when 'error'
-          'alert-danger'
-          when 'alert'
-          'alert-warning'
-          when 'notice'
-          'alert-info'
-          else
-          key.to_s
-          end
+        case key
+        when 'success'
+        'alert-success'
+        when 'error'
+        'alert-danger'
+        when 'alert'
+        'alert-warning'
+        when 'notice'
+        'alert-info'
+        else
+        key.to_s
+        end
+     end
+
+     def truncate_with_ellipsis(text, length = 50)
+        if text.length > length
+          "#{text[0...length]}..."
+        else
+          text
+        end
      end
         
 end
