e5d5a4be9eb6e5c45d1c01df9d9d5df174998ff4
zain-tx <zain@techcreatix.com>
2024-07-30 18:41:37 +0500

    fix TTT-214 & TTT-215

--- app/models/public_holiday.rb
+++ app/models/public_holiday.rb
@@ -1,2 +1,6 @@
 class PublicHoliday < ApplicationRecord
+     scope :on_date, ->(date) { where('start_date <= ? AND end_date >= ?', date, date) }
+     def self.holiday_on?(date)
+          where('start_date <= ? AND end_date >= ?', date, date).exists?
+     end
 end
