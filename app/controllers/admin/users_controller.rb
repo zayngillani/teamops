47084fad6b80d53876dc6a83b2e5b3b83e4e8876
jahanzaib-tx <jahanzaib.javed@techcreatix.com>
2024-08-28 18:28:59 +0500

    Revert "TTT-319-Re design user side dashboard page"

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -11,9 +11,6 @@ class Admin::UsersController < ApplicationController
       @user = User.new
     end
    
-    def admin_dashboard
-      
-    end
     def create
       unless params[:user][:password] == params[:user][:password_confirmation]
         flash[:error] = "Passwords don't match. Please check and re-type your confirm password."
