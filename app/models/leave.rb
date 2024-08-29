cd7f81de4da9acf84610e932a8d1d95feada412f
jahanzaib-tx <jahanzaib.javed@techcreatix.com>
2024-08-29 18:37:09 +0500

    fixed bugs in leaves

--- app/models/leave.rb
+++ app/models/leave.rb
@@ -1,5 +1,6 @@
 class Leave < ApplicationRecord
   belongs_to :user
+  validates :reason, presence: true
   enum status: [:pending, :approved, :rejected]
-  enum leave_type: {quaterly: 0 , annual: 1}
+  enum leave_type: {quarterly: 0 , annual: 1}
 end
