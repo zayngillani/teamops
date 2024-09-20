92fc7cb9a2b7956434df844f0b85cdea19150ff1
zain-tx <zain@techcreatix.com>
2024-09-20 16:59:27 +0500

    oncall restriction

--- app/controllers/oncall_support_controller.rb
+++ app/controllers/oncall_support_controller.rb
@@ -28,6 +28,18 @@ class OncallSupportController < ApplicationController
                redirect_to show_oncalls_path, flash: { error: "Oncall Already Submitted" }
                return
           end
+          approved_leaves = Leave.where(user_id: current_user.id, status: 'approved')
+               .where("start_date <= ? AND end_date >= ?", params[:end_date], params[:start_date])
+          public_holidays = PublicHoliday.where("start_date <= ? AND end_date >= ?", params[:end_date], params[:start_date])
+          (Date.parse(params[:start_date])..Date.parse(params[:end_date])).each do |date|
+          is_weekend = date.saturday? || date.sunday?
+          is_public_holiday = public_holidays.any? { |holiday| date.between?(holiday.start_date, holiday.end_date) }
+          is_approved_leave = approved_leaves.any? { |leave| date.between?(leave.start_date, leave.end_date) }
+            unless is_weekend || is_public_holiday || is_approved_leave
+              redirect_to show_oncalls_path, flash: { error: "On-call requests can only be created on weekends, approved leaves, or public holidays." }
+              return
+            end
+          end
           if @oncall.save
             flash[:success] = 'On Call Support Request Submitted'
             redirect_to show_oncalls_path
